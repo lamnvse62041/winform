8b747c3a4146996f04578eb2598d626717509278
lamnvse62041 <lamnvse62041@gmail.com>
2025-09-19 14:31:57 +0700

    update data config

--- AppConfig/AppConfig.cs
+++ AppConfig/AppConfig.cs
@@ -8,11 +8,11 @@ namespace WindowsFormsApp1.Constant
 {
     internal static class AppConfig
     {
-        //public static string connectionString = @"Server=LOCALHOST\SQLEXPRESS01;Database=DeviceIT;Trusted_Connection=True;";
+        public static string connectionString = @"Server=LOCALHOST\SQLEXPRESS01;Database=DeviceIT;Trusted_Connection=True;";
         public static string appuser = "devtest";
-        //    public static string password = "Test@123";
-            public static string connectionString =
-        @"Server=192.168.20.80;Database=DWCDevice;User Id=devtest;Password=Test@123;";
+        ////    public static string password = "Test@123";
+        //    public static string connectionString =
+        //@"Server=192.168.20.80;Database=DWCDevice;User Id=devtest;Password=Test@123;";
 
     }
 }
